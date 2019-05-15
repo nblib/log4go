24fc6afe6aeabebc368a7ec7b146f9748c818a31
hewe <tingshage@163.com>
2019-05-15 14:28:44 +0800

    ADD: tcp自动重连

--- socklog.go
+++ socklog.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -13,6 +14,7 @@ import (
 type SocketLogWriter chan *LogRecord
 
 var UDP_FAILED_WAITTIME time.Duration = 1
+var TCP_RECONN_WAITTIME time.Duration = 3
 
 // This is the SocketLogWriter's output method
 func (w SocketLogWriter) LogWrite(rec *LogRecord) {
@@ -28,31 +30,56 @@ func (w SocketLogWriter) Close() {
 
 func NewSocketLogWriter(proto, hostport string) SocketLogWriter {
 	sock, err := net.Dial(proto, hostport)
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "NewSocketLogWriter(Start connect): %s,err: %v\n", hostport, err)
-		return nil
+		//return nil
 	}
 
 	w := SocketLogWriter(make(chan *LogRecord, LogBufferLength))
 
 	go func() {
+		isTcp := strings.EqualFold(proto, "tcp")
 		defer func() {
-			if sock != nil && proto == "tcp" {
+			if sock != nil && isTcp {
 				sock.Close()
 			}
 		}()
-
+		if sock == nil {
+			//重连
+			sock = blockForReconnect(proto, hostport)
+		}
 		for rec := range w {
 			// Marshall into yyyy-MM-dd HH:mm:ss message
 			message := rec.toSTR()
 			_, err = sock.Write(message)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "SocketLogWriter(At writing): %s,err: %v", hostport, err)
-				//发送失败,等待一段时间重试
-				time.Sleep(UDP_FAILED_WAITTIME * time.Second)
+				fmt.Fprintf(os.Stderr, "SocketLogWriter(At writing): %s,err: %v \n", hostport, err)
+				//发送失败,等待一段时间重试或重连
+				if isTcp {
+					sock = blockForReconnect(proto, hostport)
+				} else {
+					//不是tcp,等待一会再试
+					time.Sleep(UDP_FAILED_WAITTIME * time.Second)
+				}
 			}
 		}
 	}()
 
 	return w
+}
+
+/**
+阻塞等待重连接
+*/
+func blockForReconnect(proto string, addr string) (sock net.Conn) {
+	var err error
+	for {
+		sock, err = net.Dial(proto, addr)
+		if err == nil {
+			return
+		} else {
+			time.Sleep(TCP_RECONN_WAITTIME * time.Second)
+		}
+	}
 }
